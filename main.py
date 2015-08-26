d19937ea34f8b31f20fced188c2ff285bdacac90
Jethro Guce <jethro.guce@myoffice.ph>
2015-08-26 21:01:35 +0800

    add quit

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ resolution = [288, 512]
 surface = pygame.display.set_mode(resolution)
 
 while True:
-	surface.fill([255, 255, 255])
+    surface.fill([255, 255, 255])
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    keystate = pygame.key.get_pressed()
+    if keystate[pygame.K_ESCAPE]:
+        pygame.quit()
