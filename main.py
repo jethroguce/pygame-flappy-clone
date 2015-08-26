518d47529ee1a38f125aeb8cc8736753f8408c49
Jethro Guce <jethro.guce@myoffice.ph>
2015-08-26 21:10:51 +0800

    draw bird

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.init()
 resolution = [288, 512]
 surface = pygame.display.set_mode(resolution)
 
+bird = pygame.image.load('assets/sprites/yellowbird-downflap.png').convert_alpha()
+
 while True:
     surface.fill([255, 255, 255])
 
@@ -16,3 +18,6 @@ while True:
     keystate = pygame.key.get_pressed()
     if keystate[pygame.K_ESCAPE]:
         pygame.quit()
+
+    player = surface.blit(bird,[0,0])
+    pygame.display.update()
