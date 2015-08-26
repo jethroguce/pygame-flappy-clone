435088593000d9b870c2b59fd795f4fce236a5c0
Jethro Guce <jethro.guce@myoffice.ph>
2015-08-26 21:34:59 +0800

    add background

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ SCREENHEIGHT = 512
 resolution = [SCREENWIDTH, SCREENHEIGHT]
 surface = pygame.display.set_mode(resolution)
 
+background = pygame.image.load('assets/sprites/background-day.png').convert()
+
 bird = pygame.image.load('assets/sprites/yellowbird-downflap.png').convert_alpha()
 playerx = int(SCREENWIDTH * 0.2)
 playery = int((SCREENHEIGHT - bird.get_height()) / 2)
@@ -15,6 +17,7 @@ playery = int((SCREENHEIGHT - bird.get_height()) / 2)
 
 while True:
     surface.fill([255, 255, 255])
+    bg = surface.blit(background,[0,0])
 
     events = pygame.event.get()
     for event in events:
@@ -29,6 +32,7 @@ while True:
 
     playery += 1/4
     player = surface.blit(bird,[playerx,playery])
+
     pygame.display.update()
     clock.tick(60)
 
